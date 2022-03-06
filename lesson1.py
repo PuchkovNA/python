af59e7d225ab9e36b399a551bb91501ce15ce767
PuchkovNA <n.puchkov@gmail.com>
2022-03-06 15:40:48 +0300

    changes

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,5 @@
-"""
-#1
+__author__ = "Пучков Николай Александрович"
+# 1
 a = 10
 print(a)
 b = 'Hello world'
@@ -7,29 +7,27 @@ print(b)
 c = int(input('Введите ваш возраст:'))
 print('Ваш возраст:', c)
 d = input('Введите ваше имя:')
-print('Ваше имя:',d)
+print('Ваше имя:', d)
 
-#2
-sec = int(input('Введите веремя в секундах:'))
-hours = sec//3600
-minutes = (sec - hours*3600)//60
-secundes = sec  - hours*3600 -minutes*60
+# 2
+sec = int(input('Введите время в секундах:'))
+hours = sec // 3600
+minutes = (sec - hours * 3600) // 60
+secundes = sec - hours * 3600 - minutes * 60
 
-print('Время в формате "чч:мм:сс" {:02d}:{:02d}:{:02d}'.format(hours,minutes,secundes))
+print('Время в формате "чч:мм:сс" {:02d}:{:02d}:{:02d}'.format(hours, minutes, secundes))
 
-#3
-num = int(input('Введите целое число:'))
-index = 0
-summa1 = num
-while index < 3:
-    print(num)
-    summa = num*10**index+summa1
-    summa1 = summa
-    print(summa)
+# 3
+num = input('Введите целое число:')
+summa = 0
+index = 1
+while index < int(num) + 1:
+    summa += int(num * index)
     index += 1
-print(num)
-"""
-#4
+print(summa)
+# 4
+print(max(list(input('Введите число:'))))
+# =====================================================
 num = input('Введите число:')
 index = 0
 cifer1 = 0
@@ -39,7 +37,15 @@ while index < len(num):
         cifer1 = cifer
     index += 1
 print(cifer1)
-
+# =======================================================
+num = int(input('Введите число:'))
+max = 0
+while num > 0:
+    division = num % 10
+    if max <= division:
+        max = division
+    num = num // 10
+print(max)
 # 5
 expenses = float(input('Введите издержки :'))
 gross = float(input('Введите выручку :'))
@@ -53,7 +59,7 @@ expenses = float(input('Введите издержки :'))
 gross = float(input('Введите выручку :'))
 if gross >= expenses:
     print('Прибыль :', gross - expenses, '.руб')
-    print('Рентабельность в %:', (gross - expenses)*100/gross)
+    print('Рентабельность в %:', (gross - expenses) * 100 / gross)
     employers = int(input('Введите количество работников :'))
     print('Прибыль в расчете на одного сотрудника:', (gross - expenses) / employers, '.руб')
 else:
@@ -64,6 +70,6 @@ a = int(input('Первый день километров :'))
 b = int(input('Цель: пробежать километров :'))
 i = 1
 while a < b:
-    a += a*0.1
-    print(i,'-й день:','{:10.2f}'.format(a))
-    i +=1
+    a += a * 0.1
+    print(i, '-й день:', '{:10.2f}'.format(a))
+    i += 1
